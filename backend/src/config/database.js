b1cf97be0786b06062ec07a44a4578d7840ec222
shiryu-tanaka <shiryu.1211@icloud.com>
2026-01-23 02:17:20 +0900

    feat: PostgreSQLに対応

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,25 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: path.join(__dirname, '../../database.sqlite'),
-    logging: false,
-});
+// 環境変数によってデータベースを入れ替える
+const sequelize = process.env.DATABASE_URL
+  ?
+    new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false    
+    })
+  :
+    new Sequelize({
+      dialect: 'sqlite',
+      storage: path.join(__dirname, '../../database.sqlite'),
+      logging: false,
+    });
 
 module.exports = sequelize;
